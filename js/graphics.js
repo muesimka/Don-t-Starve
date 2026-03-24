b788c5727e94ab152bb965e6c21ecea5af145adc
SanukuAnuku <sanukuanuku@gmail.com>
2026-03-24 13:50:23 +0300

    Update graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -14,3 +14,12 @@ window.drawTreeTrunk = function(ctx, x, y) {
     ctx.fillStyle = 'brown';
     ctx.fillRect(x - 5, y - 20, 10, 40);
 }
+// Функция для рисования верхушки кроны
+window.drawTreeTop = function(ctx, x, y) {
+    // TODO: Зелёный круг радиусом 15 в точке (x, y-30)
+    // 👇 Твой код здесь
+    ctx.fillStyle = 'green';
+    ctx.beginPath();
+    ctx.arc(x, y - 30, 15, 0, Math.PI * 2);
+    ctx.fill();
+}
