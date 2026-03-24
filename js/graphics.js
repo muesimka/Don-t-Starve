cf151e899e4184225e5ccae3be2790324297f85e
Valeev Serj <145398532+Gabryelf@users.noreply.github.com>
2026-03-24 13:54:18 +0300

    Update graphics.js

--- js/graphics.js
+++ js/graphics.js
@@ -23,3 +23,20 @@ window.drawTreeTop = function(ctx, x, y) {
     ctx.arc(x, y - 30, 15, 0, Math.PI * 2);
     ctx.fill();
 }
+
+window.drawTreeSides = function(ctx, x, y) {
+    ctx.fillStyle = 'green';
+    
+    ctx.beginPath();
+    ctx.arc(x - 10, y - 20, 12, 0, Math.PI * 2);
+    ctx.fill();
+    
+    ctx.beginPath();
+    ctx.arc(x + 10, y - 20, 12, 0, Math.PI * 2);
+    ctx.fill();
+}
+window.drawTree = function(ctx, x, y) {
+    window.drawTreeTrunk(ctx, x, y);
+    window.drawTreeTop(ctx, x, y);
+    window.drawTreeSides(ctx, x, y);
+}
