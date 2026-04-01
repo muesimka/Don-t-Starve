2a9f6c38cdb7825ff42e828562a22d3ddbab2f88
SanukuAnuku <sanukuanuku@gmail.com>
2026-04-01 11:44:40 +0300

    Update config.js

--- js/config/config.js
+++ js/config/config.js
@@ -1,1 +1,20 @@
-
+// Добавить в конец файла config.js
+window.GameAssets = {
+    images: {
+        player: 'assets/images/player.png',
+        enemy: 'assets/images/enemy.png',
+        tree: 'assets/images/tree.png',
+        berry: 'assets/images/berry.png',
+        ground: 'assets/images/ground.png',
+        heart: 'assets/images/berry.png', // временно используем ягоду для сердца
+        meat: 'assets/images/berry.png',  // временно используем ягоду для мяса
+        button: 'assets/images/button.png'
+    },
+    sounds: {
+        click: 'assets/sounds/click.mp3',
+        gather: 'assets/sounds/gather.mp3',
+        hit: 'assets/sounds/click.mp3',
+        ambient: 'assets/sounds/click.mp3',
+        gameover: 'assets/sounds/click.mp3'
+    }
+};
